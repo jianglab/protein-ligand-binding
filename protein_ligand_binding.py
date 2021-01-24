887f766c27b8194f7720c1daecb20c1c2f8eafb1
Wen Jiang <wjiang@gmail.com>
2021-01-24 12:30:57 -0500

    add P:L ratio mode

--- protein_ligand_binding.py
+++ protein_ligand_binding.py
@@ -40,14 +40,15 @@ import_with_auto_install(required_packages)
 
 import streamlit as st
 import numpy as np
+from bokeh.plotting import figure
+from bokeh.models import LegendItem
 
 def main():
     title = "Protein-Ligand Binding"
     st.set_page_config(page_title=title, layout="wide")
-    st.title(title)
 
     with st.sidebar:    # sidebar at the left of the screen
-        st.header('The Problem')
+        st.title(title)
         st.write(r'For a reversible binding reaction:$P + L\  {\rightleftharpoons}\  P{\cdot}L$')
         st.write(r"the binding affinity $K_D$ is defined as$K_D = \frac{[P][L]}{[P {\cdot}L]}$")
         st.write(r'then, the fraction of protein in ligand-bound state is:')
@@ -58,56 +59,101 @@ def main():
         st.latex(r'P_Tf^2-(P_T+L_T+K_D)f+L_T=0')
         st.latex(r'f=\frac{P_T+L_T+K_D-\sqrt{(P_T+L_T+K_D)^2-4P_TL_T}}{2P_T}')
         
+        # make radio display horizontal
+        st.markdown('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
     # right-side main panel
     col1, col2 = st.beta_columns((1, 5))
     with col1:
+        mode = st.radio(label="", options=["[P]", "[P]:[L]"], index=0)
         m = st.number_input('Protein mass (kDa)', value=100.0, min_value=10.0, step=10.0, format="%.1f")
-        n = st.number_input('# of [Protein]', value=3, min_value=1, step=1)
-        ps = [0] * n
-        for i in range(n):
-            c = 0.01 * np.power(10, i)
-            ps[i] = st.number_input(f'[Protein] - {i+1} (mg/ml)', value=c, min_value=0.0, step=0.1, format="%.3f")
-        kd = st.number_input('Kd (nM)', value=1000.0, min_value=0.0, step=1.0, format="%.3f")
-        lmin = st.number_input('[Ligand] - min (μM)', value=1e-3*kd*1e-3, min_value=0.0, step=1.0, format="%.3f")
-        lmax = st.number_input('[Ligand] - max (μM)', value=1e3*kd*1e-3, min_value=lmin*1e4, step=1.0, format="%.3f")
+        if mode == "[P]":
+            n = st.number_input('# of [P]', value=3, min_value=1, step=1)
+            ps = [0] * n
+            for i in range(n):
+                c = 0.01 * np.power(10, i)
+                ps[i] = st.number_input(f'[P] (mg/ml) - {i+1}', value=c, min_value=0.0, step=0.1, format="%.3f")
+            ps = np.unique([ p for p in ps if p>0 ])
+            kd = st.number_input('Kd (nM)', value=1000.0, min_value=0.0, step=1.0, format="%.3f")
+            p_molar_min = ps.min()/m * 1e3 # mg/ml -> μM
+            p_molar_max = ps.max()/m * 1e3 # mg/ml -> μM
+            lmin = st.number_input('[L] (μM) - min', value=p_molar_min*1e-2, min_value=0.0, step=p_molar_min*1e-2, format="%.3f")
+            lmax = st.number_input('[L] (μM) - max', value=p_molar_max*1e2, min_value=lmin*1e4, step=p_molar_max, format="%.3f")
+        else:   # "[P]:[L]"
+            n = st.number_input('# of [P]:[L]', value=3, min_value=1, step=1)
+            ratios = [1.0] * n
+            for i in range(n):
+                ratio = 1.0 * np.power(10, i)
+                ratios[i] = st.number_input(f'[P]:[L]=1:? - {i+1}', value=ratio, min_value=0.0, step=1.0, format="%.1f")
+            ratios = np.unique([ r for r in ratios if r>0 ])
+            kd = st.number_input('Kd (nM)', value=1000.0, min_value=0.0, step=1.0, format="%.3f")
+            pmin = st.number_input('[P] (mg/ml) - min', value=0.001, min_value=0.0, step=0.01, format="%.4f")
+            pmax = st.number_input('[P] (mg/ml) - max', value=10.0, min_value=pmin*10.0, step=0.1, format="%.3f")
         n_data_points = st.number_input('# of data points', value=100, min_value=10, step=10)
         xlog = st.checkbox('X-axis in log scale', value=True)
-
-    with col2:
-        ps = [ p for p in ps if p>0 ]
-        if xlog:
-            l = np.logspace(np.log10(lmin), np.log10(lmax), n_data_points+1)
-        else:
-            l = np.linspace(lmin, lmax, n_data_points+1)
-        l_molar = l * 1e-6  # μM -> M
+        
         kd_molar = kd * 1e-9    # nM -> M
 
-        from bokeh.plotting import figure
-        from bokeh.models import LegendItem
-        x_label = "[ligand] (μM)"
+    with col2:
         y_label = "fraction of protein in ligand-bound state"
         x_axis_type = "log" if xlog else "linear"
-
         tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
-        hover_tips = [("[ligand]", "$x μM"), ("fraction", "$y")]
-        fig = figure(title="", x_axis_type=x_axis_type, x_axis_label=x_label, y_axis_label=y_label, tools=tools, tooltips=hover_tips)
-
-        ll_line_dashes = 'solid dashed dotted dotdash dashdot'.split()
+        line_dashes = 'solid dashed dotted dotdash dashdot'.split()
         legends = []
         raw_data = []
-        for pi, p in enumerate(ps):
-            p_molar = p/(m*1e3) # mg/ml -> M
-            f = binding_fraction(kd=kd_molar, protein_concentration=p_molar, ligand_concentration=l_molar)
-            source = dict(x=l, y=f)
-            line_dash = ll_line_dashes[pi%len(ll_line_dashes)]
-            line_width = 2
-            line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=line_width)
-            label = f"[protein] = {float(round(p,3))} mg/ml / {float(round(p_molar*1e6,3))} μM"
-            legends.append(LegendItem(label=label, renderers=[line]))
-            raw_data.append((label, f))
+        if mode == "[P]":
+            if xlog:
+                l = np.logspace(np.log10(lmin), np.log10(lmax), n_data_points+1)
+            else:
+                l = np.linspace(lmin, lmax, n_data_points+1)
+            l_molar = l * 1e-6  # μM -> M
 
-        fig.x_range.start = l[0]
-        fig.x_range.end = l[-1]
+            x = l
+            x_label = "[L] (μM)"
+
+            hover_tips = [("[P]:[L]", "1:@ratio"), ("[P]", "@p_txt"), ("[L]", "$x μM"), ("Kd", "@kd nM"), ("fraction", "$y")]
+            fig = figure(title="", x_axis_type=x_axis_type, x_axis_label=x_label, y_axis_label=y_label, tools=tools, tooltips=hover_tips)
+
+            for pi, p in enumerate(ps):
+                p_molar = p/(m*1e3) # mg/ml -> M
+                p_txt = f"{float(round(p,3))} mg/ml / {float(round(p_molar*1e6,3))} μM"
+                ratio = (l_molar/p_molar).round(1)
+                f = binding_fraction(kd=kd_molar, protein_concentration=p_molar, ligand_concentration=l_molar)
+                source = dict(x=x, y=f, p_txt=[p_txt]*len(x), ratio=ratio, kd=[kd]*len(x))
+                line_dash = line_dashes[pi%len(line_dashes)]
+                line_width = 2
+                line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=line_width)
+                label = f"[P] = {p_txt}"
+                legends.append(LegendItem(label=label, renderers=[line]))
+                raw_data.append((label, f))        
+            fig.x_range.start = l[0]
+            fig.x_range.end = l[-1]
+        else:   # "[P]:[L]"
+            if xlog:
+                p = np.logspace(np.log10(pmin), np.log10(pmax), n_data_points+1)
+            else:
+                p = np.linspace(min, pmax, n_data_points+1)
+            p_molar = p / (m*1e3)  # mg/ml -> M
+
+            x = p
+            x_label = "[P] (mg/ml)"
+
+            hover_tips = [("[P]:[L]", "@ratio_txt"), ("[P]", "$x mg/ml / @x_mc μM"), ("[L]", "@l μM"), ("Kd", "@kd nM"), ("fraction", "$y")]
+            fig = figure(title="", x_axis_type=x_axis_type, x_axis_label=x_label, y_axis_label=y_label, tools=tools, tooltips=hover_tips)
+
+            for ri, r in enumerate(ratios):
+                ratio_txt = f"1:{float(round(r,1))}"
+                l_molar = p_molar * r
+                f = binding_fraction(kd=kd_molar, protein_concentration=p_molar, ligand_concentration=l_molar)
+                source = dict(x=x, y=f, x_mc=p_molar*1e6, ratio_txt=[ratio_txt]*len(x), l=l_molar*1e6, kd=[kd]*len(x))
+                line_dash = line_dashes[ri%len(line_dashes)]
+                line_width = 2
+                line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=line_width)
+                label = f"[P]:[L] = {ratio_txt}"
+                legends.append(LegendItem(label=label, renderers=[line]))
+                raw_data.append((label, f))
+            fig.x_range.start = p[0]
+            fig.x_range.end = p[-1]        
         fig.y_range.start = 0
         fig.y_range.end = 1
         fig.yaxis[0].ticker.desired_num_ticks = 10
@@ -133,8 +179,8 @@ def main():
         show_data = st.checkbox('show raw data', value=False)
         if show_data:
             import pandas as pd
-            data = np.zeros((len(l), 1+len(raw_data)))
-            data[:, 0] = l
+            data = np.zeros((len(x), 1+len(raw_data)))
+            data[:, 0] = x
             columns = [x_label]
             for i, (label, f) in enumerate(raw_data):
                 data[:, 1+i] = f
@@ -173,7 +219,7 @@ def setup_anonymous_usage_tracking():
         index_file.chmod(stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IROTH)
         txt = index_file.read_text()
         if txt.find("gtag/js?")==-1:
-            txt = txt.replace("<head>", '''<head><script async src="https://www.googletagmanager.com/gtag/js?id=G-8Z99BDVHTC"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-8Z99BDVHTC');</script>''')
+            txt = txt.replace("<head>", '''<head><script async src="https://www.googletagmanager.com/gtag/js?id=G-6ZB67M09Q9"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-6ZB67M09Q9');</script>''')
             index_file.write_text(txt)
     except:
         pass
