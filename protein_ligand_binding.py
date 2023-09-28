dde6cb810df501edf0bf44497df19b28d687017d
Wen Jiang <jiang12@purdue.edu>
2023-09-27 20:02:26 -0400

    cleanup

--- protein_ligand_binding.py
+++ protein_ligand_binding.py
@@ -1,46 +1,19 @@
-def import_with_auto_install(packages, scope=locals()):
-    if isinstance(packages, str): packages=[packages]
-    for package in packages:
-        if package.find(":")!=-1:
-            package_import_name, package_pip_name = package.split(":")
-        else:
-            package_import_name, package_pip_name = package, package
-        try:
-            scope[package_import_name] = __import__(package_import_name)
-        except ImportError:
-            import subprocess
-            subprocess.call(f'pip install {package_pip_name}', shell=True)
-            scope[package_import_name] =  __import__(package_import_name)
-required_packages = "streamlit numpy pandas bokeh".split()
-import_with_auto_install(required_packages)
-
-from secrets import randbelow
-from ssl import PROTOCOL_TLS_CLIENT
 import streamlit as st
 import numpy as np
-from bokeh.plotting import figure
-from bokeh.models import LegendItem 
 import pandas as pd
-from bokeh.models import CustomJS
-from bokeh.events import MouseEnter
-from bokeh.models import Legend
-from bokeh.events import DoubleTap
+from bokeh.plotting import figure
+from bokeh.models import LegendItem, CustomJS, Legend 
+from bokeh.events import MouseEnter, DoubleTap
 from sympy import nsolve
 from sympy.abc import H
-
-
-
 
 def main():
     title = "Single Ligand Multi-Protein Binding"
     st.set_page_config(page_title=title, layout="wide")
 
-    hosted, host = is_hosted(return_host=True)
-    if hosted and host in ['heroku']:
-        st.error(f"This app hosted on Heroku will be unavailable starting November 28, 2022 [when Heroku discontinues free hosting service](https://blog.heroku.com/next-chapter). Please switch to [the same app hosted elsewhere](https://jianglab-protein-ligand-binding-streamlit-app-7qd88x.streamlitapp.com)")
+    st.title(title)
 
-    with st.sidebar:
-        st.title(title)
+    with st.expander("README"):
         st.write(r'For a competitive binding reaction:$\\P_{A} + L\  {\rightleftharpoons}\  P_{A}{\cdot}L$ $\\P_{B} + L\  {\rightleftharpoons}\  P_{B}{\cdot}L$')
         st.write(r'the binding affinities, $K_A$ and $K_B$, are defined as  $K_D = \frac{[P]_{free}[L]_{free}}{[P {\cdot}L]}$.')
         st.write(r'Then, by equations 1-3, the concentration of protein, $P_{A}$, in a competitive ligand-bound state is:')
@@ -58,29 +31,25 @@ def main():
         """
         st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-    col1, col2 = st.columns((1.5, 4.5))
-
+    col1, col2 = st.sidebar, st.columns((1, 0.001))[0]
 
     with col1:
         
         num_p = st.number_input('Number of Proteins', value=3, min_value=1, step=1)
 
-        column_p = ['Protein %d' % (i+1) for i in range(int(num_p))]
+        column_p = [f'P{i+1}' for i in range(int(num_p))]
         df = pd.DataFrame([np.ones(num_p), [0.001*10**(i*2) for i in range(int(num_p))]], ['Prot. Conc. (μM)', 'Kd (μM)'], column_p)
         edited_df = st.data_editor(df) 
 
-        st.divider()
-
-        lmax = st.number_input('Maximal Ligand Concentration (μM)', value=1000, min_value=1, step=1000)
-        lmin = st.number_input('Minimal Ligand Concentration (μM)', value=0.001, min_value=0.0, format = '%g', step=0.01)
-        
+        df = pd.DataFrame([[0.001, 1000]], ['Ligand Conc. (μM)'], ["Min", "Max"])
+        edited_df_ligand = st.data_editor(df) 
+        lmin = edited_df_ligand.iloc[0, 0]
+        lmax = edited_df_ligand.iloc[0, 1]
 
         xlog = st.checkbox('X-axis in log scale', value=True)
 
-
-        
-
-
+        st.markdown("  \n")
+        st.markdown("*Developed by the [Jiang Lab@Purdue University](https://jiang.bio.purdue.edu/multi-binding). Report problems to [multi-binding@GitHub](https://github.com/jianglab/protein-ligand-binding/issues)*")
 
     with col2:
         y_label = "Fraction of protein in ligand-bound state"
@@ -91,7 +60,6 @@ def main():
         raw_data = []
         i = 0
         ratios = [1.0] * int(num_p)
-
         
         if int(num_p) == 1:
             if xlog:
@@ -108,18 +76,18 @@ def main():
             for ri, r in enumerate(ratios):
                 frac = H
                 i=i+1
-                p_mol = edited_df.loc['Prot. Conc. (μM)', f'Protein {i}'] 
-                kd_molar = edited_df.loc['Kd (μM)', f'Protein {i}'] 
+                p_mol = edited_df.loc['Prot. Conc. (μM)', f'P{i}'] 
+                kd_molar = edited_df.loc['Kd (μM)', f'P{i}'] 
                 kd_list_molar = pd.concat([edited_df.iloc[1][0:i-1],edited_df.iloc[1][i:]])
                 p_mol_list = pd.concat([edited_df.iloc[0][0:i-1],edited_df.iloc[0][i:]])
-                ratio_txt = f"{edited_df.loc['Prot. Conc. (μM)', f'Protein {i}']}"
+                ratio_txt = f"{edited_df.loc['Prot. Conc. (μM)', f'P{i}']}"
                 frac += one_bind(kd=kd_molar, prot_conc=p_mol, H=H)
                 sol_l=[]
                 for j in l:
                     tmp=frac-j
                     sol_l.append(nsolve(tmp, (0,j), solver = 'bisect'))
                 sol_l=np.array(sol_l,dtype=np.float64)
-                total = (one_bind(kd=kd_molar, prot_conc=p_mol, H = sol_l))/(edited_df.loc['Prot. Conc. (μM)', f'Protein {i}'])
+                total = (one_bind(kd=kd_molar, prot_conc=p_mol, H = sol_l))/(edited_df.loc['Prot. Conc. (μM)', f'P{i}'])
                 source = dict(x=x, y=total, ratio_txt=[ratio_txt]*len(x), kd=[kd_molar]*len(x))
                 line_dash = line_dashes[ri%len(line_dashes)]
                 line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=2)
@@ -128,7 +96,6 @@ def main():
                 raw_data.append((label, total))
             fig.x_range.start = l[0]
             fig.x_range.end = l[-1]
-
 
         else:
             if xlog:
@@ -145,18 +112,18 @@ def main():
             for ri, r in enumerate(ratios):  
                 frac = H
                 i=i+1
-                p_mol = edited_df.loc['Prot. Conc. (μM)', f'Protein {i}'] 
-                kd_molar = edited_df.loc['Kd (μM)', f'Protein {i}'] 
+                p_mol = edited_df.loc['Prot. Conc. (μM)', f'P{i}'] 
+                kd_molar = edited_df.loc['Kd (μM)', f'P{i}'] 
                 kd_list_molar = pd.concat([edited_df.iloc[1][0:i-1],edited_df.iloc[1][i:]])
                 p_mol_list = pd.concat([edited_df.iloc[0][0:i-1],edited_df.iloc[0][i:]])
-                ratio_txt = f"{edited_df.loc['Prot. Conc. (μM)', f'Protein {i}']}"
+                ratio_txt = f"{edited_df.loc['Prot. Conc. (μM)', f'P{i}']}"
                 frac += percent_bound(kd=kd_molar, kd_list=kd_list_molar, prot_conc = p_mol, prot_conc_list=p_mol_list, H=H)
                 sol_l=[]
                 for j in l:
                     tmp = frac-j
                     sol_l.append(nsolve(tmp, (0,j), solver = 'bisect'))
                 sol_l = np.array(sol_l,dtype=np.float64)
-                total = (percent_bound(kd=kd_molar, kd_list=kd_list_molar, prot_conc = p_mol, prot_conc_list=p_mol_list, H = sol_l))/(edited_df.loc['Prot. Conc. (μM)', f'Protein {i}'])
+                total = (percent_bound(kd=kd_molar, kd_list=kd_list_molar, prot_conc = p_mol, prot_conc_list=p_mol_list, H = sol_l))/(edited_df.loc['Prot. Conc. (μM)', f'P{i}'])
                 source = dict(x=x, y=total, ratio_txt=[ratio_txt]*len(x), kd=[kd_molar]*len(x))
                 line_dash = line_dashes[ri%len(line_dashes)]
                 line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=2)
@@ -165,7 +132,6 @@ def main():
                 raw_data.append((label, total))
             fig.x_range.start = l[0]
             fig.x_range.end = l[-1]
-
 
         fig.y_range.start = 0
         fig.y_range.end = 1
@@ -190,7 +156,6 @@ def main():
             fig.js_on_event(DoubleTap, toggle_legend_js)
         st.text("") 
         st.bokeh_chart(fig, use_container_width=True)
- 
 
 def one_bind(kd, prot_conc, H):
     total = (prot_conc*H)/(kd+H)
@@ -199,33 +164,6 @@ def one_bind(kd, prot_conc, H):
 def percent_bound(kd, kd_list, prot_conc, prot_conc_list, H):
     total = (prot_conc*H)/(kd+H)
     return total
-
-def get_username():
-    from getpass import getuser
-    return getuser()
-
-def get_hostname():
-    import socket
-    fqdn = socket.getfqdn()
-    return fqdn
-
-def is_hosted(return_host=False):
-    hosted = False
-    host = ""
-    fqdn = get_hostname()
-    if fqdn.find("heroku")!=-1:
-        hosted = True
-        host = "heroku"
-    username = get_username()
-    if username.find("appuser")!=-1:
-        hosted = True
-        host = "streamlit"
-    if not host:
-        host = "localhost"
-    if return_host:
-        return hosted, host
-    else:
-        return hosted
 
 if __name__ == "__main__":
     main()
